b91d09b62d04cf65cd435819395f7fb7f5ac67f8
mikoloism <mikoloism@gmail.com>
2023-12-09 22:13:57 +0330

    feat(pkg/Auth): dependency inversion in `mod.ts`
    - re-export all routers and needed stuff
    - export `AuthModule` function with give `app` as `express.Application`
      as interface to use with `.use` middleware methods to assign `routers`
      and `controllers` and `handlers`

--- packages/Auth/libs/mod.ts
+++ packages/Auth/libs/mod.ts
@@ -1,1 +1,9 @@
-export * as router from './router'
+import type { Application } from 'express';
+import * as router from './router';
+
+async function AuthModule(app: Application): Promise<void> {
+	app.use(router.Auth);
+}
+
+export * as router from './router';
+export default AuthModule;
