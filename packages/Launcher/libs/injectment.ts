a43642cb5393e09373a0234731739ad92fa31e3c
mikoloism <mikoloism@gmail.com>
2023-12-11 00:50:49 +0330

    refactor(pkg/Launcher): improve sources code with oop-principles
    - remove `replace` function
    - add base abstract class for `injectors` implemented from `injectable` interface contract
    - implement `database` injector
    - extract constants
    - migrate to Deno runtime
    - NOTE: `npm:glob` have error to cache dependency and should be replaced with alternative

--- packages/Launcher/libs/injectment.ts
+++ packages/Launcher/libs/injectment.ts
@@ -1,21 +1,35 @@
-import * as glob from 'glob';
-import * as fs from 'node:fs';
+import glob from 'npm:glob';
 
-function replace(content: string, pattern: string | RegExp, target: string): string {
-	return content.replace(new RegExp(pattern, 'gm'), target);
+interface Injectable {
+	inject(content: string): string;
+}
+
+abstract class BaseInjector implements Injectable {
+	protected abstract _pattern: RegExp;
+	protected abstract _injectment: string;
+	public inject(content: string): string {
+		return content.replace(this._pattern, this._injectment);
+	}
+}
+
+class DatabaseInjector extends BaseInjector {
+	protected _pattern = /\$libs\/db/gm;
+	protected _injectment = 'real-path-to/db';
 }
 
 const SOURCE_GLOB_PATTERN = '**/*.{tsx,ts,jsx,js,astro,svelte}';
-const LIBS_DB_PATTERN = /\$libs\/db/;
-const LIBS_DB_REPLACEMENT = 'to-path/db';
 
 async function main() {
 	const files = glob.sync(SOURCE_GLOB_PATTERN, { nodir: true });
-	files.map(function mapFileToInject(file) {
-		const content = fs.readFileSync(file, 'utf-8');
-		const injected = replace(content, LIBS_DB_PATTERN, LIBS_DB_REPLACEMENT);
-		fs.writeFileSync(file, injected, 'utf-8');
-	});
+	const dbInjector = new DatabaseInjector();
+
+	await Promise.all(
+		files.map(async function mapFileToInject(file: string) {
+			const content = await Deno.readTextFile(file);
+			const injected = dbInjector.inject(content);
+			await Deno.writeTextFile(file, injected);
+		}),
+	);
 }
 
 main().catch(function exception(error: unknown) {
