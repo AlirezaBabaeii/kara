b0bca3a7a8372ae62e81dcb622942dcea1d72625
mikoloism <mikoloism@gmail.com>
2023-12-13 17:28:45 +0330

    refactor(pkg/Auth): migrate Deno and OAK
    - return a promise
    - improve `router` using in as middleware

--- packages/Auth/libs/mod.ts
+++ packages/Auth/libs/mod.ts
@@ -1,8 +1,9 @@
 import type { Application } from 'https://deno.land/x/oak@v12.6.1/mod.ts';
 import * as router from './router.ts';
 
-async function AuthModule(app: Application): Promise<void> {
-	app.use(router.Auth);
+function AuthModule(app: Application): Promise<void> {
+	app.use(router.AuthLogin.routes(), router.AuthLogin.allowedMethods() /*  , AuthLoginImpl */);
+	return Promise.resolve();
 }
 
 export * as router from './router.ts';
